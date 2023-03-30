92fee5111fa301bffc9cd10f9d7406fb7ee98fb0
Spoty3 <122308479+Spoty3@users.noreply.github.com>
2023-03-30 21:31:15 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('contextmenu', function(e){
+    e.preventDefault();
+    
+});
+var images = document.getElementsByTagName('img');
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener('dragstart', function(event) {
+    event.preventDefault();
+  });
+}
